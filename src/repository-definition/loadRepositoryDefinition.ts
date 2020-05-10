cd78bec1c72a7f63ca1d23fedde3a66a7bb8f245
Drew Kimberly <akimberl@redhat.com>
2020-05-10 10:47:27 -0400

    fix: Use async fs.readFile when reading repository definitions

--- src/repository-definition/loadRepositoryDefinition.ts
+++ src/repository-definition/loadRepositoryDefinition.ts
@@ -1,10 +1,14 @@
 import {RepositoryDefinition} from './types';
 import * as yaml from 'js-yaml';
 import * as fs from 'fs';
+import * as util from 'util';
 import {Maybe} from '../util';
+
+const readFile = util.promisify(fs.readFile);
 
 export const loadRepositoryDefinition = async (
   repositoryDefinitionPath: string
 ): Promise<Maybe<RepositoryDefinition>> => {
-  return yaml.load(fs.readFileSync(repositoryDefinitionPath).toString());
+  const file = await readFile(repositoryDefinitionPath);
+  return await yaml.load(file.toString());
 };
